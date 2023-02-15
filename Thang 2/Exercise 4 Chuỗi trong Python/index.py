d9427d22218d4fba504db080658426e25aca3575
PHAM MINH ANH <minhanh.itqn@gmail.com>
2023-02-15 15:54:45 +0900

    task thang 2

---
+++ Thang 2/Exercise 4 Chuỗi trong Python/index.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+print("Website \a Toidicode.com  ")
+#xuống dòng
+print("Website \a Toidicode.com  ")
+#có tiếng chuông khi thực hiện dòng này
+print("Website \t Toidicode.com  ")
+#tab
+print("Website         \b Toidicode.com  ")
+# xóa bỏ một  khoảng  trắng
+
+# format chuỗi
+char = 10
+print("The character is: %c" % char)
---
+++ Thang 2/Exercise 5 So sánh list Va tuple/index.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+# Tạo một list
+my_list = [1, 2, 3, 4]
+print("xuong dong  \n",my_list)
+
+# Chuyển list thành tuple
+my_tuple = tuple(my_list)
+
+# In kết quả
+print(my_tuple) # (1, 2, 3, 4)
+print("B \n",my_tuple)
